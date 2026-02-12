90eac19449f5ee0a8c9aa10dc409a3ca612dc543
anatoliyserebry <anatoliyserebry@gmail.com>
2026-02-11 20:10:46 -1200

    Fix WPF converter namespace resolution compatibility

---
+++ CryptoTrackFinal/Converters/LegacyNamespaceConverters.cs
@@ -0,0 +1,15 @@
+namespace CryptoTrackFinal.Converters
+{
+    // Backward-compatible converter aliases for older XAML namespace declarations.
+    public class BoolToVisibilityConverter : CryptoTrackClient.Converters.BoolToVisibilityConverter
+    {
+    }
+
+    public class ProfitLossColorConverter : CryptoTrackClient.Converters.ProfitLossColorConverter
+    {
+    }
+
+    public class PriceChangeColorConverter : CryptoTrackClient.Converters.PriceChangeColorConverter
+    {
+    }
+}
